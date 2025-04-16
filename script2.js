f29e728d04f28b57c11d064a64875aa2b89a14e9
kinroslonska <kin.calka@gmail.com>
2025-04-16 04:11:25 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,61 @@
+const people = [
+  { firstName: false, lastName: 2 },
+  { firstName: "Roman", lastName: "Kowalski" },
+  { firstName: "Halina", lastName: "Malina" },
+  { firstName: "B", lastName: "22" },
+  { firstName: "Jan", lastName: "Nowak" },
+  { firstName: "Kamil", lastName: null }
+];
+
+function generatePseudonyms(people) {
+  return people.map(person => {
+    const { firstName, lastName } = person;
+
+    if (
+      typeof firstName !== 'string' ||
+      typeof lastName !== 'string' ||
+      firstName.trim().length < 3 ||
+      lastName.trim().length < 3
+    ) {
+      return person;
+    }
+
+    const reversedFirst = firstName.trim().slice(-3).split('').reverse().join('');
+    const reversedLast = lastName.trim().slice(0, 3).split('').reverse().join('');
+    const pseudonym = (reversedFirst + reversedLast)
+      .toLowerCase()
+      .replace(/^./, c => c.toUpperCase());
+
+    return { ...person, pseudonym };
+  });
+}
+
+function generateAgeData(peopleWithPseudonyms) {
+  return peopleWithPseudonyms
+    .filter(person => person.pseudonym)
+    .map((person, index) => {
+      const { firstName, lastName, pseudonym } = person;
+
+      const nameLength = firstName.length + lastName.length;
+
+      let age;
+
+      if (nameLength % 2 === 0) {
+        age = nameLength;
+      } else {
+        const totalLength =
+          ["firstName", "lastName", "pseudonym"]
+            .reduce((sum, key) => sum + person[key].length, 0);
+        const safeIndex = index === 0 ? 1 : index;
+        age = Math.ceil(totalLength / safeIndex);
+      }
+
+      return { ...person, age };
+    });
+}
+
+const withPseudonyms = generatePseudonyms(people);
+const finalResult = generateAgeData(withPseudonyms);
+
+console.log(finalResult);
+
